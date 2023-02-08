965f476046e0eec1767d24b5d48ce0cca25a9f2c
felipe.salles <fsalles@detran.ms.gov.br>
2023-02-08 15:01:03 -0400

    Chamada http via ajax

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -1,6 +1,7 @@
 // funçao para capturar as metatags
 $("#linkPromocao").on('change', function() {
 	var url = $(this).val();
+	var error = false;
 
 	// no minimo http://
 	if (url.length > 7) {
@@ -8,8 +9,33 @@ $("#linkPromocao").on('change', function() {
 			method : "POST",
 			url : "/meta/info?url=" + url,
 			cache : false,
+			beforeSend : function() {
+				$("#alert").removeClass("alert alert-danger").text("");
+				$("#titulo").val("");
+				$("#site").text("");
+				$("#linkImagem").attr("src", "");
+				$("#loader-img").addClass("loader");
+			},
 			success : function(data) {
 				console.log(data);
+				$("#titulo").val(data.title);
+				$("#site").text(data.site);
+				$("#linkImagem").attr("src", data.image.replace("undefinedxundefined", "800x560"));
+			},
+			statusCode : {
+				404 : function() {
+					$("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperada dessa URL.");
+					error = true;
+				}
+			},
+			error : function() {
+				$("#alert").addClass("alert alert-danger").text("Ops... algo deu errado, tente novamento mais tarde.");
+				error = true;
+			},
+			complete : function() {
+				$("#loader-img").removeClass("loader");
+				if (error)
+					$("#linkImagem").attr("src", "/images/promo-dark.png");
 			}
 		})
 	}
