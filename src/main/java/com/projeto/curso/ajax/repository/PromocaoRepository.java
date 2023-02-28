3e3f8fa4c4ac9c7ef1cec64ff3e1054f9b6a88e3
felipe.salles <fsalles@detran.ms.gov.br>
2023-02-28 13:02:12 -0400

    Controle de likes

--- src/main/java/com/projeto/curso/ajax/repository/PromocaoRepository.java
+++ src/main/java/com/projeto/curso/ajax/repository/PromocaoRepository.java
@@ -1,9 +1,21 @@
 package com.projeto.curso.ajax.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projeto.curso.ajax.domain.Promocao;
 
-public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
+public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+
+	@Query("select p.likes from Promocao p where p.id=:id")
+	int findLikesById(@Param("id") Long id);
+
+	@Modifying
+	@Transactional(readOnly = false)
+	@Query("update Promocao p set p.likes = p.likes + 1 where p.id=:id")
+	void updateSomarLikes(@Param("id") Long id);
 
 }
--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -47,3 +47,19 @@ function loadByScrollBar(pageNumber) {
 		}
 	});
 }
+
+// controle de likes
+$(document).on("click", "button[id*='likes-btn-']", function() {
+	var id = $(this).attr("id").split("-")[2];
+
+	$.ajax({
+		method : "POST",
+		url : "/promocao/like/" + id,
+		success : function(response) {
+			$("#likes-count-" + id).text(response);
+		},
+		error : function(xhr) {
+			alert("Ops ocorreu um erro " + xhr.status + " - " + xhr.statusText)
+		}
+	})
+});
