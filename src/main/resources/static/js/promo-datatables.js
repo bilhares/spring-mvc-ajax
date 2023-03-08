ff7baa36d1bb0663002c64cb835e06410262568b
felipe.salles <fsalles@detran.ms.gov.br>
2023-03-08 09:05:32 -0400

    Edição e exclusao na datatable

--- src/main/resources/static/js/promo-datatables.js
+++ src/main/resources/static/js/promo-datatables.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	moment.locale("pt-br");
 
-	$("#table-server").DataTable({
+	var table = $("#table-server").DataTable({
 		processing : true,
 		serverSide : true,
 		responsive : true,
@@ -36,7 +36,7 @@ $(document).ready(function() {
 			}
 		}, {
 			data : "preco",
-			render : $.fn.dataTable.render.number('.', ',', 2, 'R$')
+			render : $.fn.dataTable.render.number(".", ",", 2, "R$")
 		}, {
 			data : "likes"
 		}, {
@@ -46,10 +46,53 @@ $(document).ready(function() {
 			}
 		}, {
 			data : "categoria.titulo"
+		} ],
+		dom : "Bfrtip",
+		buttons : [ {
+			text : "Editar",
+			attr : {
+				id : "btn-editar",
+				type : "button"
+			},
+			enabled : false
+		}, {
+			text : "Excluir",
+			attr : {
+				id : "btn-excluir",
+				type : "button"
+			},
+			enabled : false
 		} ]
 	});
-});
 
-function formatText(text) {
-	return text != "" && text.length >= 40 ? text.substring(0, 40) + "..." : text
-}
+	// desmarcar botao ao ordenar
+	$("#table-server thead").on("click", "tr", function() {
+		table.buttons().disable();
+	});
+
+	// marcar e desmarcar botoes
+	$("#table-server tbody").on("click", "tr", function() {
+		if ($(this).hasClass("selected")) {
+			$(this).removeClass("selected");
+			table.buttons().disable();
+		} else {
+			$("tr.selected").removeClass("selected");
+			$(this).addClass("selected");
+			table.buttons().enable();
+		}
+	});
+
+	$("#btn-editar").on("click", function() {
+		var id = table.row(table.$("tr.selected")).data().id;
+		console.log("id " + id);
+	});
+
+	$("#btn-excluir").on("click", function() {
+		console.log("excluir");
+	});
+
+	function formatText(text) {
+		return text != "" && text.length >= 40 ? text.substring(0, 40) + "..." : text
+	}
+
+});
